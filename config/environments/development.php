9dccbe905ece66d1579a3c9a2654daea34964eb5
Kevin McKernan <kevin@mckernan.in>
2017-05-27 09:28:49 -0600

    replace s3 keys

--- config/environments/development.php
+++ config/environments/development.php
@@ -4,6 +4,6 @@ define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
 define( 'S3_UPLOADS_BUCKET', 'tahosawp' );
-define( 'S3_UPLOADS_KEY', 'AKIAIJD5HM4AU6RNDV4Q' );
-define( 'S3_UPLOADS_SECRET', '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' );
+define( 'S3_UPLOADS_KEY', 'AKIAIIMADUSJOGB44BCA' );
+define( 'S3_UPLOADS_SECRET', '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' );
 define( 'S3_UPLOADS_REGION', 'us-west-2' );
