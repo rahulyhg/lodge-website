3a6e32fff166caa08e8c0174cbe679a69704718b
Kevin McKernan <kevin@mckernan.in>
2017-05-27 09:26:26 -0600

    add s3 config

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,3 +3,7 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+define( 'S3_UPLOADS_BUCKET', 'tahosawp' );
+define( 'S3_UPLOADS_KEY', 'AKIAIJD5HM4AU6RNDV4Q' );
+define( 'S3_UPLOADS_SECRET', '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' );
+define( 'S3_UPLOADS_REGION', 'us-west-2' );
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -5,3 +5,8 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+
+define( 'S3_UPLOADS_BUCKET', 'tahosawp' );
+define( 'S3_UPLOADS_KEY', 'AKIAIJD5HM4AU6RNDV4Q' );
+define( 'S3_UPLOADS_SECRET', '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' );
+define( 'S3_UPLOADS_REGION', 'us-west-2' );
