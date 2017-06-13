adeb99377f1381d0bdb695a502800d0f87d84c86
Kevin McKernan <kevin@mckernan.in>
2017-06-13 16:53:26 -0600

    Work in progress

---
+++ web/app/plugins/cuboree-registration/templates/breadcrumbs.php
@@ -0,0 +1,7 @@
+<div class="et_pb_section cuboree-guide">
+    <div class="container">
+        <div class="et_pb_extra_row">
+            <span><a href="/cuboree">Add Tickets to Cart</a></span> <span class="sep">↠</span> <span><a href="/cart">View and Confirm Cart</a></span> <span class="sep">↠</span> <span><a href="/checkout">Checkout</a></span>
+        </div>
+    </div>
+</div>
