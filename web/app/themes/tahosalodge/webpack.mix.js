efa7fed4ace1b723fb365ca0612fcf4b6f10b66e
Kevin McKernan <kevin@mckernan.in>
2017-06-16 00:01:50 -0600

    Mix/Webpack tweaks

--- web/app/themes/tahosalodge/webpack.mix.js
+++ web/app/themes/tahosalodge/webpack.mix.js
@@ -1,7 +1,47 @@
-const { mix } = require('laravel-mix');
+let mix = require('laravel-mix');
 
-mix.js('assets/js/app.js', 'assets/dist')
-    .sass('assets/scss/style.scss', './').options({
-        processCssUrls: false
-    })
-    .browserSync('https://tahosalodge.dev');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ */
+
+ mix.autoload({
+    jquery: ['$', 'window.jQuery']
+ });
+mix.js('assets/js/app.js', 'dist/');
+mix.sass('assets/scss/style.scss', './');
+mix.options({
+  processCssUrls: false
+});
+mix.disableNotifications();
+mix.browserSync('https://tahosalodge.dev');
+mix.webpackConfig({
+  externals: {
+    jquery: 'window.jQuery'
+  }
+});
+
+
+// Full API
+// mix.js(src, output);
+// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
+// mix.less(src, output);
+// mix.stylus(src, output);
+// mix.browserSync('my-site.dev');
+// mix.combine(files, destination);
+// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
+// mix.copy(from, to);
+// mix.copyDirectory(fromDir, toDir);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.setResourceRoot('prefix/for/resource/locators');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
