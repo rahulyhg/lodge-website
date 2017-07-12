77017490ab424997d54525b2bb25a1f6c87e5755
Kevin McKernan <kevin@mckernan.in>
2017-07-12 09:26:43 -0600

    fix conditional ticket fields in ES6

--- web/app/themes/tahosalodge/assets/js/modules/cuboree.js
+++ web/app/themes/tahosalodge/assets/js/modules/cuboree.js
@@ -1,10 +1,11 @@
 import Cookie from 'js-cookie';
 
 export default function ($) {
-  $('.product_cat-cuboree').on('change', '.do-you-have-any-dietary-restrictions input', () => {
-    const $requiredItem = $(this).closest('.form-row').siblings('.dietary-restrictions');
+  $('.product_cat-cuboree').on('change', '.do-you-have-any-dietary-restrictions input', (e) => {
+    const $this = $(e.currentTarget);
+    const $requiredItem = $this.closest('.form-row').siblings('.dietary-restrictions');
     $requiredItem.toggle();
-    $requiredItem.children('input').prop('required', this.checked);
+    $requiredItem.children('input').prop('required', $this.checked);
   });
 
   $('[type="number"]').keypress((e) => {
