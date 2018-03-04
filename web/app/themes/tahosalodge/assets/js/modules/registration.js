2ecd94532e8bc7088fe7c2ea0d63443336b746a7
Kevin McKernan <kevin@mckernan.in>
2018-03-04 13:45:51 -0700

    upgrade mix, add dietary restrictions js for registrations

--- web/app/themes/tahosalodge/assets/js/modules/registration.js
+++ web/app/themes/tahosalodge/assets/js/modules/registration.js
@@ -1,14 +1,20 @@
 import Cookie from 'js-cookie';
 
-export default function ($) {
-  $('.product_cat-cuboree').on('change', '.do-you-have-any-dietary-restrictions input', (e) => {
-    const $this = $(e.currentTarget);
-    const $requiredItem = $this.closest('.form-row').siblings('.dietary-restrictions');
-    $requiredItem.toggle();
-    $requiredItem.children('input').prop('required', $this.checked);
-  });
+export default function($) {
+  $('.product').on(
+    'change',
+    '.do-you-have-any-allergies-or-dietary-restrictions input',
+    event => {
+      const $this = $(event.currentTarget);
+      const $requiredItem = $this
+        .closest('.form-row')
+        .siblings('.dietary-restriction-details');
+      $requiredItem.toggle();
+      $requiredItem.children('input').prop('required', $this.checked);
+    }
+  );
 
-  $('[type="number"]').keypress((e) => {
+  $('[type="number"]').keypress(e => {
     const a = [];
     const k = e.which;
     let i = 48;
