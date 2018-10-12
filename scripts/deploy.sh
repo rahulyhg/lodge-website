5fb6d63419edb895643291aba481fa9f50a4bfb6
Kevin McKernan <kevin@mckernan.in>
2018-10-11 21:36:09 -0600

    debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-if [ "$CIRCLE_BRANCH" = 'develop' ]
-  then
+#!/bin/bash -xe
+# if [ "$CIRCLE_BRANCH" = 'develop' ]
+#   then
     ENVIRONMENT="staging"
     PATH="~/apps/tahosalodge-staging"
-fi
-if [ "$CIRCLE_BRANCH" = 'master' ]
-  then
-    ENVIRONMENT="production"
-    PATH="~/apps/tahosalodge"
-fi
-
+# fi
+# if [ "$CIRCLE_BRANCH" = 'master' ]
+  # then
+    # ENVIRONMENT="production"
+    # PATH="~/apps/tahosalodge"
+# fi
+which rsync
 rsync -avP --delete ./ tahosalodge@tahosa.co:$PATH
 
-if [ $? -eq 0 ]; then
-    echo 'Site deployed.'
-fi
+# if [ $? -eq 0 ]; then
+    # echo 'Site deployed.'
+# fi
 
